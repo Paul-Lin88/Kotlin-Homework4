9a3340446fb10b6d98a10f1276137bc8ef6fec70
Paul-Lin88 <t109360116@ntut.org.tw>
2022-12-24 00:39:40 +0800

    first commit

---
+++ Lab12/app/src/main/java/com/example/lab12/MyService.kt
@@ -0,0 +1,27 @@
+package com.example.lab12
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+class MyService : Service() {
+    override fun onCreate() {
+        super.onCreate()
+        GlobalScope.launch {
+            delay(3000)
+            try {
+                val intent = Intent(this@MyService, SecActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            }
+            catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+    }
+    override fun onStartCommand(intent: Intent,
+                                flags: Int, startid: Int): Int {
+        return START_NOT_STICKY}
+    override fun onBind(intent: Intent): IBinder? = null
+}
